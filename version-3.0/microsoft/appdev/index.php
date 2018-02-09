91df8ffc4483728fa9b2cbd29c5e696b7feed208
mganti <mganti@miraclesoft.com>
2018-02-09 10:34:56 -0500

    title changed in microsoft

--- version-3.0/microsoft/appdev/index.php
+++ version-3.0/microsoft/appdev/index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title>Microsoft Technologies | Miracle</title>
+    <title>Microsoft | Miracle</title>
     <link href="../../../css/fix-media-query.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="../../../css/default.css" />
 	<link rel="stylesheet" type="text/css" href="../../../css/component.css" />
